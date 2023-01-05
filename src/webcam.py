be4f7513d5bf7b4379008660e7f942031565bf8e
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-01-05 16:35:54 -0300

    Add face following

--- src/webcam.py
+++ src/webcam.py
@@ -1,5 +1,7 @@
+import os
 import time
 from threading import Event
+from typing import Optional, Tuple
 
 import cv2
 import numpy as np
@@ -57,13 +59,32 @@ class VirtualWebcam:
             self.virtual_webcam_path, self.width, self.height
         )
         self.running = False
-        self.consumers = 0
         self.stop_event = Event()
 
         self.sepia_intensity = 0.5
 
+        self._frame_counter_for_mask = 0
+        self._frame_counter_for_face = 0
+
+        self.mask_frame_limit = 10
+        self.face_frame_limit = 30
+
+        self.mask = None
+        self.face: Optional[Tuple[int, int, int, int]] = None
+        classifier_path = os.path.join(
+            os.path.dirname(__file__), "assets", "haarcascade_frontalface_default.xml"
+        )
+        self.classifier = (
+            cv2.CascadeClassifier(classifier_path)
+            if os.path.exists(classifier_path)
+            else None
+        )
+
     def _send_to_virtual_webcam(self, frame):
-        self.virtual_webcam.schedule_frame(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        try:
+            self.virtual_webcam.schedule_frame(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        except Exception as e:
+            print(e)
 
     def start(self):
 
@@ -75,6 +96,14 @@ class VirtualWebcam:
             if self.running:
                 frame = self.webcam.read()
                 frame = self._render_image(frame)
+                if self._frame_counter_for_mask > self.mask_frame_limit:
+                    self._frame_counter_for_mask = 0
+                else:
+                    self._frame_counter_for_mask += 1
+                if self._frame_counter_for_face > self.face_frame_limit:
+                    self._frame_counter_for_face = 0
+                else:
+                    self._frame_counter_for_face += 1
             else:
                 frame = self.blank_frame
                 self.webcam.close()
@@ -106,18 +135,57 @@ class VirtualWebcam:
 
     def _render_image(self, frame: NDArray[np.uint8]) -> NDArray[np.uint8]:
         cv2.resize(frame, (self.width, self.height))
-
-        mask = self.engine.process(frame).segmentation_mask
-
-        cv2.threshold(mask, 0.9, 1, cv2.THRESH_BINARY, dst=mask)
-        cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1, dst=mask)
-        cv2.blur(mask, (10, 10), dst=mask)
-        cv2.accumulateWeighted(mask, self.old_mask or mask, 0.5)
-
+        self._compute_mask(frame)
         background_frame = self._apply_blur(frame)
         background_frame = self._apply_sepia(background_frame)
-        cv2.blendLinear(frame, background_frame, mask, 1 - mask, dst=frame)
+        if self.classifier:
+            self._detect_face(frame)
+        cv2.blendLinear(frame, background_frame, self.mask, 1 - self.mask, dst=frame)
+        frame = self._crop_face(frame)
         return frame
+
+    def _compute_mask(self, frame: NDArray[np.uint8]) -> None:
+
+        if (
+            self._frame_counter_for_mask != self.mask_frame_limit
+            and self.mask is not None
+        ):
+            return
+
+        self.mask = self.engine.process(frame).segmentation_mask
+        cv2.threshold(self.mask, 0.9, 1, cv2.THRESH_BINARY, dst=self.mask)
+        cv2.dilate(self.mask, np.ones((5, 5), np.uint8), iterations=1, dst=self.mask)
+        cv2.blur(self.mask, (10, 10), dst=self.mask)
+        cv2.accumulateWeighted(self.mask, self.old_mask or self.mask, 0.5)
+
+    def _detect_face(self, frame: NDArray[np.uint8]) -> None:
+
+        if (
+            self._frame_counter_for_face != self.face_frame_limit
+            and self.face is not None
+        ):
+            return
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.classifier.detectMultiScale(gray, 1.1, 6)
+        if len(faces) == 0:
+            return
+        if len(faces) == 1:
+            self.face = faces[0]
+        max_area = max(w * h for (x, y, w, h) in faces)
+        face = next((x, y, w, h) for (x, y, w, h) in faces if w * h == max_area)
+        self.face = face
+
+    def _crop_face(self, frame: NDArray[np.uint8]) -> NDArray[np.uint8]:
+        if self.face is not None:
+            x, y, w, h = self.face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
+            n_h = int(2 * h)
+            n_w = int(n_h * (self.width / self.height))
+            n_x = int(x - (n_w - w) / 2)
+            n_y = int(y - (n_h - h) / 2)
+            frame = frame[n_y : n_y + n_h, n_x : n_x + n_w]
+            return cv2.resize(frame, (self.width, self.height))
 
     def _apply_blur(self, frame: NDArray[np.uint8]) -> NDArray[np.uint8]:
         return cv2.GaussianBlur(
